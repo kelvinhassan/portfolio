e837f9b40629b54cf40de1de0e3005ec2c5c5639
kelvinhassan <ngerajustin@gmail.com>
2023-09-26 06:02:26 +0300

    Personal Portfolio first commit, Navbar setup complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react"
+import "./App"
+import { Routes, Route } from "react-router-dom"
+import { Home, About, Resume, Skills, Portfolio, Contact } from "./containers"
+import Navbar from "./components/navBar/Navbar"
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* particles.js */}
+      {/* navbar.js */}
+      <Navbar />
+      {/* main pagevcontent*/}
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/containers/index.js
@@ -0,0 +1,8 @@
+import About from "./about/About"
+import Contact from "./contact/Contact"
+import Home from "./home/Home"
+import Portfolio from "./portfolio/Portfolio"
+import Resume from "./resume/Resume"
+import Skills from "./skills/Skills"
+
+export { About, Contact, Home, Portfolio, Resume, Skills }
