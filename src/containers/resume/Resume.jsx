7ff449ada970d8bc2a724e049235fc42e18ed0cc
kelvinhassan <ngerajustin@gmail.com>
2023-09-28 12:33:28 +0300

    resume skills and education

--- src/containers/resume/Resume.jsx
+++ src/containers/resume/Resume.jsx
@@ -1,20 +1,92 @@
 import React from "react"
-import { BsInfoCircleFill } from "react-icons/bs"
+import { FaUserTie } from "react-icons/fa"
 import PageHeaderContent from "../../components/pageHeaderContent/PageHeaderContent"
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component"
+import { data } from "./utils"
+import "react-vertical-timeline-component/style.min.css"
+import "./style.scss"
+import { MdWork } from "react-icons/md"
 
 const Resume = () => {
   return (
     <section id="resume" className="resume">
       <PageHeaderContent
         headerText="My Resume"
-        icon={<BsInfoCircleFill size={40} />}
+        icon={<FaUserTie size={40} />}
       />
       <div className="timeline">
         <div className="timeline__experience">
           <h3 className="timeline__experience__header-text">Experience</h3>
+          <VerticalTimeline
+            layout={"1-column"}
+            lineColor="var(--yellow-theme-main-color)"
+          >
+            {data.experience.map((item, i) => (
+              <VerticalTimelineElement
+                key={i}
+                className="timeline__experience__verticle-timeline-element"
+                contentStyle={{
+                  background: "none",
+                  color: "var(--yellow-theme-sub-text-color)",
+                  border: "1.5px solid var(--yellow-theme-main-color)",
+                }}
+                icon={<MdWork />}
+                iconStyle={{
+                  background: "#181818",
+                  color: "var(--yellow-theme-main-color)",
+                }}
+              >
+                <div className="verticle-timeline-element-title-wrapper">
+                  <h3>{item.title}</h3>
+                  <h4>{item.subTitle}</h4>
+                </div>
+                <p className="verticle-timeline-element-title-wrapper-description">
+                  {item.description}
+                </p>
+                <p className="verticle-timeline-element-title-wrapper-description">
+                  {item.date}
+                </p>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
         </div>
         <div className="timeline__education">
-          <h3 className="timeline__education__header-text">Experience</h3>
+          <h3 className="timeline__education__header-text">Education</h3>
+          <VerticalTimeline
+            layout={"1-column"}
+            lineColor="var(--yellow-theme-main-color)"
+          >
+            {data.education.map((item, i) => (
+              <VerticalTimelineElement
+                key={i}
+                className="timeline__experience__verticle-timeline-element"
+                contentStyle={{
+                  background: "none",
+                  color: "var(--yellow-theme-sub-text-color)",
+                  border: "1.5px solid var(--yellow-theme-main-color)",
+                }}
+                icon={<MdWork />}
+                iconStyle={{
+                  background: "#181818",
+                  color: "var(--yellow-theme-main-color)",
+                }}
+              >
+                <div className="verticle-timeline-element-title-wrapper">
+                  <h3>{item.title}</h3>
+                  <h4>{item.subTitle}</h4>
+                </div>
+                <p className="verticle-timeline-element-title-wrapper-description">
+                  {item.description}
+                </p>
+                <p className="verticle-timeline-element-title-wrapper-description">
+                  {item.date}
+                </p>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
         </div>
       </div>
     </section>
--- src/containers/resume/utils.js
+++ src/containers/resume/utils.js
@@ -2,24 +2,35 @@ export const data = {
   experience: [
     {
       title: "IT Consultant",
-      subTitle: "Nairobi, KE",
-      description: " user Experience, Web development, Databases",
+      subTitle: "Entach-Tech: Nairobi, KE",
+      description: "Web development, User Support, User Experience, Databases",
+      date: "SEP 2021 - Present",
     },
     {
-      title: "IT Trainer",
-      subTitle: "KIAMBU, KE",
+      title: "ICT Trainer",
+      subTitle: "KIST: KIAMBU, KE",
       description: "Lecturing, Tutoring, Mentorship",
+      date: "SEP 2021 - AUG 2023",
     },
     {
-      title: "ICT TECHNICIAN",
+      title: "ICT Technician",
       subTitle: "KIAMBU, KE",
       description:
         "User Support, Network setup and management, Firewall Management, System Administration, Team Leading",
+      date: "SEP 2015 - SEP 2021",
     },
     {
       title: "Systems Developer",
       subTitle: "Nairobi, KE",
       description: " Backend Development, Databases, User support",
+      date: "MAR 2014 - JUN 2015",
+    },
+    {
+      title: "ICT Officer",
+      subTitle: "KNH: Nairobi, KE",
+      description:
+        "Network Installation, User Support, Hardware Maintenance, Software installation",
+      date: "FEB 2013 - FEB 2014",
     },
   ],
   education: [
@@ -28,32 +39,38 @@ export const data = {
       subTitle: "Mount Kenya University",
       description:
         "Bachelor of Science in Information Technology, Second class Upper",
+      date: "SEP 2016 - DEC 2020",
     },
     {
       title: "University",
       subTitle: "Mount Kenya University",
       description: "Diploma in Business Information Technology, Credit 1",
+      date: "JAN 2011 - DEC 2013",
     },
     {
       title: "Certification",
       subTitle: "ALX Africa",
       description:
         "ALX Software Engineering with a specialization in Backend Development",
+      date: "MAR 2022 - MAY 2023",
     },
     {
       title: "Certification",
       subTitle: "UDACITY -> AWS",
       description: "AWS Cloud Practitioner, AWS Machine Learning Foundation",
+      date: "NOV 2022",
     },
     {
       title: "Certification",
       subTitle: "HUAWEI",
       description: "HCIA - Security",
+      date: "JUNE 2021",
     },
     {
       title: "Certification",
       subTitle: "Oracle",
       description: "Oracle Cloud Infrastracture Foundations",
+      date: "FEB 2022",
     },
   ],
 }
